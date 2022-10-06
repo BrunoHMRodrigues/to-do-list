800f7c054a2c58ac7c84a3c90eb058bb1159833d
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-06 13:04:11 -0300

    Realizado Requsito Bônus 12, alteração na lógica para uso do innerHTML

--- script.js
+++ script.js
@@ -76,29 +76,33 @@ buttonClearCompleted.addEventListener("click", removeCompletedTasks);
 const buttonSaveTasks = document.getElementById("salvar-tarefas");
 
 function saveTasks () {
-    const listItems = document.querySelectorAll("ol li");
-    let arrayTasks = [];
+    // const listItems = document.querySelectorAll("ol li");
+    // let arrayTasks = [];
 
-    for (let index = 0; index < listItems.length; index+=1) {
-        arrayTasks[index] = listItems[index];        
-    }
-    localStorage.setItem("taskList", JSON.stringify(arrayTasks));
+    // for (let index = 0; index < listItems.length; index+=1) {
+    //     arrayTasks[index] = listItems[index];        
+    // }
+    // localStorage.setItem("taskList", JSON.stringify(arrayTasks));
+
+    const list = olList.innerHTML;
+    localStorage.setItem("taskList", JSON.stringify(list));
+
 }
 buttonSaveTasks.addEventListener("click", saveTasks);
 
 function retrieveStoragedTasks () {
     const savedTasks = JSON.parse(localStorage.getItem("taskList"));
+    olList.innerHTML = savedTasks;
+    // console.log(savedTasks);
+    // if (savedTasks !== null) {
 
-    console.log(savedTasks);
-    if (savedTasks !== null) {
-
-        for (let index = 0; index < savedTasks.length; index+=1) {
-            const listItem = document.createElement("li")
-            listItem = savedTasks[index]; // como pegar as propriedades do local storage? criar 2 storage? criar um sotore por propriedade que quero salvar?
-            console.log(listItem);
-            olList.appendChild(listItem);
-        }        
-    }
+    //     for (let index = 0; index < savedTasks.length; index+=1) {
+    //         const listItem = document.createElement("li")
+    //         listItem = savedTasks[index]; // como pegar as propriedades do local storage? criar 2 storage? criar um sotore por propriedade que quero salvar?
+    //         console.log(listItem);
+    //         olList.appendChild(listItem);
+    //     }        
+    // }
 }
 
 function initiate () {
