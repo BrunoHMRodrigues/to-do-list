0e0093fcecefc42c7291860cb6e4bc67eb4348d0
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-07 10:54:54 -0300

    Realizado Requisito Bônus 13 e 14

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const buttonNewTask = document.getElementById("criar-tarefa");
 
-function addTaskToList () {
+function addTaskToList() {
     const inputBox = document.getElementById("texto-tarefa")
-    const textInput = inputBox.value;    
+    const textInput = inputBox.value;
     const fatherList = document.getElementById("lista-tarefas");
     const newLiItem = document.createElement("li");
 
@@ -15,10 +15,10 @@ function addTaskToList () {
 buttonNewTask.addEventListener("click", addTaskToList);
 
 const olList = document.getElementById("lista-tarefas");
-function clickListItem (event) {
+function clickListItem(event) {
     const itemList = event.target;
 
-    clearSelected ();
+    clearSelected();
 
     if (itemList.className === "completed") {
         itemList.className = "selected completed";
@@ -28,16 +28,16 @@ function clickListItem (event) {
 }
 olList.addEventListener("click", clickListItem);
 
-function clearSelected () {
+function clearSelected() {
     const selectedItem = document.querySelector(".selected");
 
     if (selectedItem) {
         selectedItem.classList.remove("selected");
     }
-    
+
 }
 
-function markTask (event) {
+function markTask(event) {
     const itemList = event.target;
 
     if (itemList.className === "selected completed") {
@@ -47,46 +47,83 @@ function markTask (event) {
         console.log("entrei");
         itemList.className = "selected completed"
     }
-    
+
 }
 olList.addEventListener("dblclick", markTask)
 
 const buttonClear = document.getElementById("apaga-tudo");
 
-function clearList () {
+function clearList() {
     const allLiItems = olList.childElementCount;
 
-    for (let index = 0; index < allLiItems; index+=1) {
-        olList.firstChild.remove();        
-    }    
+    for (let index = 0; index < allLiItems; index += 1) {
+        olList.firstChild.remove();
+    }
 }
 buttonClear.addEventListener("click", clearList);
 
 const buttonClearCompleted = document.getElementById("remover-finalizados");
 
-function removeCompletedTasks () {
+function removeCompletedTasks() {
     const completedItems = document.querySelectorAll(".completed");
 
-    for (let index = 0; index < completedItems.length; index+=1) {
-        completedItems[index].remove();        
+    for (let index = 0; index < completedItems.length; index += 1) {
+        completedItems[index].remove();
     }
 }
 buttonClearCompleted.addEventListener("click", removeCompletedTasks);
 
 const buttonSaveTasks = document.getElementById("salvar-tarefas");
 
-function saveTasks () {
+function saveTasks() {
     const list = olList.innerHTML;
     localStorage.setItem("taskList", JSON.stringify(list));
 }
 buttonSaveTasks.addEventListener("click", saveTasks);
 
-function retrieveStoragedTasks () {
+function retrieveStoragedTasks() {
     const savedTasks = JSON.parse(localStorage.getItem("taskList"));
-    olList.innerHTML = savedTasks;    
+    olList.innerHTML = savedTasks;
 }
 
-function initiate () {
-    retrieveStoragedTasks ();
+function initiate() {
+    retrieveStoragedTasks();
 }
-window.onload = initiate ();
+window.onload = initiate();
+
+const buttonMoveUp = document.getElementById("mover-cima");
+
+function moveUp() {
+    const itemSelected = document.querySelector(".selected");
+
+    if (itemSelected) {
+        if (itemSelected.previousElementSibling) {
+            itemSelected.parentNode.insertBefore(itemSelected, itemSelected.previousElementSibling);
+        }        
+    }
+}
+buttonMoveUp.addEventListener("click", moveUp);
+
+const buttonMoveDown = document.getElementById("mover-baixo");
+
+function moveDown() {
+    const itemSelected = document.querySelector(".selected");
+
+    if (itemSelected) {
+        if (itemSelected.nextElementSibling) {
+            itemSelected.parentNode.insertBefore(itemSelected.nextElementSibling, itemSelected);
+        }        
+    }
+}
+buttonMoveDown.addEventListener("click", moveDown);
+
+const buttonRemove = document.getElementById("remover-selecionado");
+
+function removeSelected() {
+    const itemSelected = document.querySelector(".selected");
+
+    if (itemSelected) {
+        itemSelected.remove();
+    }
+}
+buttonRemove.addEventListener("click", removeSelected);
